d3b35d2eb9604af165e658beb5f0d97c1287c66a
FrdRzls <faridrizal1111@gmail.com>
2024-06-06 22:22:22 +0700

    perubahan pada kode routes.js

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,13 @@ import HOME from '../views/pages/home';
 import HIDDEN_GEM from '../views/pages/hiddenGem';
 import Pariwisata from '../views/pages/pariwisata';
 import About from '../views/pages/about';
-
+import DETAIL from '../views/pages/detail'; 
 const routes = {
   '/': HOME,
   '/hidden-gem': HIDDEN_GEM,
   '/pariwisata': Pariwisata,
   '/about': About,
+  '/detail/:id': DETAIL, 
 };
 
 export default routes;
